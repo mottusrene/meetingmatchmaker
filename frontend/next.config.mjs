4e317b5127edc715ac2d98f44dabf9053fb99c8e
René <rene.mottus@ut.ee>
2026-03-08 17:37:38 +0000

    slight tweaks to get past a network error

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: '/api-proxy/:path*',
+        destination: 'http://backend:8000/:path*',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
